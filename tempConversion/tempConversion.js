ad584339404b3789bfabf92925627b6c57c57a4d
kmb5 <20390225+kmb5@users.noreply.github.com>
2020-10-10 14:35:34 +0200

    solved all challenges

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
+const ftoc = function(f) {
 
+  return round((f - 32) * (5/9), 1);
 }
 
-const ctof = function() {
+const ctof = function(c) {
 
+  return round(c * (9/5) + 32, 1);
 }
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
 
 module.exports = {
   ftoc,
